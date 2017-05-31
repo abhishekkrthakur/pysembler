c5934059275828d0fd195fcb5f21906a203be148
Abhishek Thakur <abhishek4@gmail.com>
2017-05-31 15:33:49 +0200

    update init

---
+++ pysembler/__init__.py
@@ -0,0 +1,1 @@
+from .ensembler import Ensembler
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pysembler',
-      version='0.01',
+      version='0.02',
       description='A small ensembler',
       author='Abhishek Thakur',
       author_email='abhishek4@gmail.com',
